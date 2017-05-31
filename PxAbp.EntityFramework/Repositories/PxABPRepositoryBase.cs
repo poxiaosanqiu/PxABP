10aa83ea62aa06e45a4ccd403f1a92d555a9f0c3
ttschinatxws <ttschinatxws@163.com>
2017-05-31 17:47:00 +0800

    自定义仓储

---
+++ PxAbp.EntityFramework/Repositories/PxABPRepositoryBase.cs
@@ -0,0 +1,35 @@
+﻿using Abp.Domain.Entities;
+using Abp.EntityFramework;
+using Abp.EntityFramework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMpaAbp.EntityFramework.Repositories
+{
+    public abstract class PxABPRepositoryBase<TEntity, TPrimaryKey> : EfRepositoryBase<PxAbpDbContext, TEntity, TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
+    {
+        protected PxABPRepositoryBase(IDbContextProvider<PxAbpDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+
+        }
+
+        //add common methods for all repositories
+    }
+
+    public abstract class PxABPRepositoryBase<TEntity> : EfRepositoryBase<TEntity, int>
+        where TEntity : class, IEntity<int>
+    {
+        protected PxABPRepositoryBase(IDbContextProvider<PxAbpDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+
+        }
+
+        //do not add any method here, add to the class above (since this inherits it)
+    }
+}
